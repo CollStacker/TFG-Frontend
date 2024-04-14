d187b3b39fc60f0dbbcb9fd161e2227b9611e7a3
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-04-14 22:29:53 +0100

    defining grey background on every view

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -58,8 +58,9 @@ import Footer from '@/components/menu/Footer.vue';
 }
 
 .collectionsMainContent {
-  margin-top: 69px;
-  margin-bottom: 50px;
+  margin: 69px 0 50px;
+  background-color: #f3f2f2;
+  min-height: calc(100vh - 69px - 50px);
 }
 
 body {
