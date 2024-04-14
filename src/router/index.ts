d211990e8551263fd324d13f5016322753baa3fb
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-04-14 10:49:27 +0100

    Defining the routes of the application

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Overview from '@/views/Overview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +7,43 @@ const router = createRouter({
     {
       path: '/',
       name: 'homeView',
-      component: Overview
+      component: HomeView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/main',
+      name: 'mainPage',
+      component: () => import('../views/Overview.vue')
+    },
+    {
+      path: '/userProfile',
+      name: 'userProfile',
+      component: () => import('../views/UserProfile.vue')
+    },
+    {
+      path: '/collections',
+      name: 'collections',
+      component: () => import('../views/Collections.vue')
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: () => import('../views/Messages.vue')
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/Settings.vue')
+    },
+    {
+      path: '/legalNotice',
+      name: 'legalNotice',
+      component: () => import('../views/LegalNotice.vue')
+    },
+    {
+      path: '/privacyPolicities',
+      name: 'privacyPolicities',
+      component: () => import('../views/PrivacyPolicies.vue')
+    },
   ]
 })
 
