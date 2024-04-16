5cb4ac0ac212a8817e4f4797b89282efbfc427e5
Adrian-glz01 <alu0101404813@ull.edu.es>
2024-04-16 20:14:34 +0100

    starting styles from user profile component

--- src/components/UserProfileComponent.vue
+++ src/components/UserProfileComponent.vue
@@ -1,23 +1,45 @@
 <template>
-  <div class="padding">
-    <div class="col-md-8">
-      <!-- Column -->
-      <div class="card"> 
-        <img class="card-img-top" src="https://i.imgur.com/K7A78We.jpg" alt="Card image cap">
-        <div class="card-body little-profile text-center">
-          <div class="pro-img"><img src="https://i.imgur.com/8RKXAIV.jpg" alt="user"></div>
-          <h3 class="m-b-0">Brad Macullam</h3>
-          <div class="row text-center m-t-20">
-            <div class="col-lg-4 col-md-4 m-t-20">
-              <h3 class="m-b-0 font-light">10434</h3><small>Articles</small>
+  <div class="userProfileComponentContainer">
+    <div class="userProfileComponentColumn">
+      <div class="userProfileComponentCard"> 
+        <img class="cardImgTop" src="../assets/imgs/sidebar/sidebar.jpg" alt="Card image cap">
+        <div class="cardBody littleProfile textCenter">
+          <div class="proImg">
+            <img v-if="userData.profilePhoto === 'maleYoung'"  src="../assets/imgs/profilePhoto/male-young.jpg"/>
+            <img v-if="userData.profilePhoto === 'maleAdult'"  src="../assets/imgs/profilePhoto/male-adult.jpg"/>
+            <img v-if="userData.profilePhoto === 'maleOld'"  src="../assets/imgs/profilePhoto/male-old.jpg"/>
+            <img v-if="userData.profilePhoto === 'femaleOld'"  src="../assets/imgs/profilePhoto/female-old.jpg"/>
+            <img v-if="userData.profilePhoto === 'femaleAdult'"  src="../assets/imgs/profilePhoto/female-adult.jpg"/>
+            <img v-if="userData.profilePhoto === 'femaleYoung'"  src="../assets/imgs/profilePhoto/female-young.jpg"/>
+          </div>
+          <span class="usernameText" >{{userData.username}}</span>
+          <div class="userDataContainer">
+            <div class="userDataText">
+              <span>Email</span>
+              <div class="emailText">
+                <InputText v-model="userData.email" disabled />
+              </div>
             </div>
-            <div class="col-lg-4 col-md-4 m-t-20">
-              <h3 class="m-b-0 font-light">434K</h3><small>Followers</small>
+            <div class="userDataText">
+              <span>Name</span>
+              <div class="emailText">
+                <InputText v-model="userData.name" disabled />
+              </div>
             </div>
-            <div class="col-lg-4 col-md-4 m-t-20">
-              <h3 class="m-b-0 font-light">5454</h3><small>Following</small>
+            <div class="userDataText">
+              <span>Surnames</span>
+              <div class="emailText">
+                <InputText v-model="userData.surnames" disabled />
+              </div>
+            </div>
+            <div class="userDataText">
+              <span>Biography</span>
+              <div class="emailText">
+                <Textarea v-model="userData.biography" disabled autoResize />
+              </div>
             </div>
           </div>
+
         </div>
       </div>
     </div>
@@ -25,30 +47,40 @@
 </template>
 
 <script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
+import { ref } from "vue";
 
+const userData = ref({
+  username: "Adrian01",
+  email: "adrian@test.com",
+  name: "Adrián",
+  surnames: "González Expósito",
+  profilePhoto: "maleOld",
+  biography: "Hola soy Adrián, actualmente estudio ingenieria informatica. Me gsuta coleccionar coches antiguos"
+})
 </script>
 
-<style scoped>
-  body {
-    background-color: #000000;
+<style>
+  
+  .userProfileComponentContainer {
     margin: 0;
     padding: 0;
     font-family: Arial, sans-serif;
-  }
-
-  .padding {
     padding: 3rem;
-    margin-left: 200px;
+    display: flex;
+    justify-content: center; 
+    align-items: center;
   }
 
-  .card {
+  .userProfileComponentCard {
     border-radius: 4px;
     background-color: #ffffff;
     margin-bottom: 30px;
     box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.05);
   }
 
-  .card-img-top {
+  .cardImgTop {
     width: 100%;
     height: 300px;
     object-fit: cover;
@@ -56,35 +88,25 @@
     border-top-right-radius: 4px;
   }
 
-  .card-body {
+  .cardBody {
     padding: 1.25rem;
   }
 
-  .little-profile .pro-img img {
-    width: 128px;
-    height: 128px;
+  
+  .littleProfile .proImg img {
+    width: 230px;
+    margin-top: -160px;
+    height: 230px;
+    margin-bottom: 10px;
     border-radius: 50%;
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.8);
   }
 
-  .text-center {
+  .textCenter {
     text-align: center;
   }
 
-  .m-b-0 {
-    margin-bottom: 0;
-  }
-
-  .m-t-20 {
-    margin-top: 20px;
-  }
-
-  .col-md-4 {
-    flex: 0 0 auto;
-    width: 33.333333%;
-  }
-
-  .col-md-8 {
+  .userProfileComponentColumn {
     flex: 0 0 auto;
     width: 66.666667%;
   }
@@ -94,4 +116,32 @@
       margin-left: 0;
     }
   }
+
+  .usernameText {
+    font-family: 'Pacifico', cursive;
+    font-size: 34px;
+    color: #2c2c2c; 
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); 
+  }
+
+  .userDataContainer {
+  display: flex;
+  flex-wrap: wrap; /* Permite que los elementos se envuelvan en varias filas */
+  /* justify-content: flex-start;  */
+  max-width: 600px; /* Ancho máximo del contenedor padre */
+}
+
+.userDataText {
+  flex: 1 1 200px; /* Establece un tamaño flexible para los elementos, con un ancho mínimo de 200px */
+  margin-right: 20px; /* Espacio entre cada columna */
+  margin-bottom: 20px; /* Espacio entre cada fila */
+}
+
+.userDataText span {
+  font-weight: bold;
+}
+
+.userDataText .emailText {
+  margin-top: 5px; /* Espacio entre el texto y el campo de entrada */
+}
 </style>
