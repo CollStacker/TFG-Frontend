4413c9f39cbcd101bbaac48282651cc2cba16674
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-04-26 23:18:48 +0100

    finishing styles in collection form view

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,4 +1,4 @@
-export interface CategoryInterface {
+export default interface CategoryInterface {
   _id?: string
   name: string
   description?: string
--- src/views/CollectionForm.vue
+++ src/views/CollectionForm.vue
@@ -3,32 +3,54 @@
     <SideBar class="sidebar"></SideBar>
     <div class="collectionsFormContent">
       <NavBar class="customNavBar"></NavBar>
-      <div class ="collectionsFormMainContent">
-        <div class="collectionFormContainer">
-          <div class="collectionform mb-8">
-            <h1 class="uppercase bold big-text">Create your collection</h1>
-            <div class="separator mb-8"></div>
-          </div>
-          <div>
-            <div class="collectionform mb-8">
-              <span class="uppercase bold"><label class="red-text">* </label>Title</span>
-              <InputText class="biggerInputText" v-model="collectionFormData.title" placeholder="Title"/>
-            </div>
-            <div class="collectionform mb-8">
-              <span class="uppercase bold">description</span>
-              <Textarea class="biggerTextArea" v-model="collectionFormData.description" placeholder="Description" :autoResize="true"/>
-            </div>
-            <div class="collectionform mb-8">
-              <Toast />
-              <span class="uppercase bold">Collection image</span>
-              <FileUpload name="demo[]" url="/api/upload" @upload="onAdvancedUpload()" :multiple="true" accept="image/*" :maxFileSize="1000000">
-                <template #empty>
-                  <p>Drag and drop files to here to upload.</p>
+      <div class="collectionsFormMainContent"> 
+        <div class=" collectionFormContainer">
+          <Toast/>
+          <div class="container" :class="{ 'right-panel-active': collectionForm }">
+            <!-- CollectionForm form -->
+            <div class="form-container sign-up-container">
+              <form @submit.prevent="createCollection">
+                <template id="registerFormStep1" v-if="collectionFormStep === 0">
+                  <div class="collectionFormHeader">
+                    <h1 class="uppercase bold big-text">Create Collection</h1>
+                    <div class="separator mb-15"></div>
+                  </div>
+                  <div class="collectionform mb-8">
+                    <div class="title-wrapper">
+                      <span class="uppercase bold fs-b"><label class="red-text">* </label>Title</span>
+                      <InputText class="biggerInputText" v-model="collectionFormData.title" placeholder="Title"/>
+                      <small>Choose a creative name, remember you can change it later</small>
+                    </div>
+                  </div>
+                  <i class="customNextStepButton pi pi-arrow-right" @click="handleNextStep"></i>
                 </template>
-              </FileUpload>
+                <template id="registerFormStep2" v-if="collectionFormStep === 1">
+                  <div class="collectionform mb-8">
+                    <div class="title-wrapper">
+                      <span class="uppercase bold fs-b">Collection image</span>
+                      <FileUpload name="demo[]" url="/api/upload" @upload="onAdvancedUpload()" :multiple="true" accept="image/*" :maxFileSize="1000000">
+                        <template #empty>
+                          <p>Drag and drop files to here to upload.</p>
+                        </template>
+                      </FileUpload>
+                    </div>
+                  </div>
+                  <div class="button-container">
+                    <i class="customPreviousStepButton pi pi-arrow-left" @click="previousCollectionStep"></i>
+                    <!-- create collection button-->
+                    <Button class="createCollectionButton pi pi-check" label="Create" type="submit"></Button>
+                  </div>
+                </template>
+              </form>
             </div>
-            <div class="collectionform mb-8">
-              
+            <div class="overlay-container">
+              <div class="overlay">
+                <!-- Left panel-->
+                <div class="overlay-panel overlay-left">
+                  <h1>Hey there, Treasure Hunter!</h1>
+                  <p>Let's create a collection together, give it a name and if you wish, insert an image</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -42,37 +64,51 @@ import SideBar from '@/components/menu/SideBar.vue';
 import NavBar from '@/components/menu/NavBar.vue';
 import Footer from '@/components/menu/Footer.vue';
 import InputText from 'primevue/inputtext';
-import Textarea from 'primevue/textarea';
+import Toast from 'primevue/toast';
+import Button from 'primevue/button';
 import FileUpload from 'primevue/fileupload';
+import InputSwitch from 'primevue/inputswitch';
 import { ref } from 'vue';
 import { userAuthentication } from '@/store/userAuth.store';
 import { useRouter } from 'vue-router';
 import { useToast } from "primevue/usetoast";
-
 
 const toast = useToast();
 const router = useRouter();
 const authStore = userAuthentication();
 
 const userData = authStore.getUserData();
+const collectionForm = ref(true);
 
 const collectionFormData = ref({
   title: '',
   description: '',
   frontPage: '',
   ownerId: userData.id,
-})
+});
 
-const collectionCategoriesData = ref([])
-const categoryData = ref ({
-  name: '',
-  collectionId: '',
-})
+const createCollection = () => {
+
+}
 
 const onAdvancedUpload = () => {
   toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
 };
 
+const collectionFormStep = ref(0);
+const handleNextStep = () => {
+  if(collectionFormStep.value === 0) {
+    if (collectionFormData.value.title == '' || collectionFormData.value.title.length == 0) {
+      toast.add({ severity: 'error', summary: 'Error Message', detail: 'Name is a mandatory field.', life: 3000 });
+    } else {
+      collectionFormStep.value += 1;
+    }
+  }
+}
+
+const previousCollectionStep = () => {
+  collectionFormStep.value -= 1;
+}
 </script>
 
 <style>
@@ -138,15 +174,44 @@ body {
 
 textarea.p-inputtextarea.p-inputtext.p-component.biggerTextArea,
 .p-inputtext.p-component.biggerInputText {
-  width: 80rem;
+  width: 18rem;
+  max-width: 18rem;
+  height: 2.5rem;
+  margin: 0px;
+  border: 1px solid #c7c7c7;
+  background-color: transparent;
+}
+
+.mt-10 {
+  margin-top: 10px;
+}
+
+.p-inputtext.p-component.addCategoryInputText {
+  width: calc(640px - 87.61px);
   max-width: 40rem;
   margin: 0px;
   border: 1px solid #c7c7c7;
   background-color: transparent;
 }
 
+.p-inputtext.p-component.showCategoryInputText {
+  width: calc(640px - 52px - 52px);
+  max-width: 40rem;
+  margin: 0px;
+  border: 1px solid #c7c7c7;
+  background-color: transparent;
+}
+
+.p-inputtext.p-component.showCategoryInputText:disabled {
+  background-color: #f2f2f2;
+}
+
 .mb-8 {
   margin-bottom: 8px;
+}
+
+.mb-15 {
+  margin-bottom: 15px;
 }
 
 .red-text {
@@ -163,13 +228,24 @@ textarea.p-inputtextarea.p-inputtext.p-component.biggerTextArea,
 
 .collectionform {
   display: flex;
-  flex-direction: column;
+  justify-content: center; 
+}
+
+.title-wrapper {
+  display: grid;
+  grid-template-columns: 1fr; 
+  grid-gap: 5px; 
+}
+
+.collectionform span {
+  text-align: left; 
 }
 
 .p-button.p-component,
 .p-button.p-component.p-fileupload-choose {
   background-color: #333;
   border: 2px solid #333;
+  color: white;
 }
 
 .p-button.p-component:hover,
@@ -188,5 +264,219 @@ textarea.p-inputtextarea.p-inputtext.p-component.biggerTextArea,
 .big-text {
   font-size: 30px;
 }
+
+.mt-13 {
+  margin-top: 13px;
+}
+
+.p-inputswitch.p-highlight .p-inputswitch-slider {
+  background: #333;
+}
+
+.p-button-label {
+  font-family:"Inter var", sans-serif;
+}
+
+.p-button.p-component.customEditCategoryButton span {
+  display: none;
+}
+
+.p-button.p-component.createCollectionButton {
+  margin-left: 206px;
+}
+
+.fs-b {
+  font-size: 18px;
+}
+</style>
+
+<style scoped>
+
+form {
+  background-color: #FFFFFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  text-align: center;
+}
+.container {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
+      0 10px 10px rgba(0,0,0,0.22);
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  max-width: 1100px; 
+  min-height: 650px;
+  position: absolute; 
+  top: 51%;
+  left: 50%;
+  transform: translate(-50%, -50%); 
+}
+
+.form-container {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+}
+
+
+.sign-up-container {
+  left: 0;
+  width: 50%;
+  opacity: 0;
+  z-index: 1;
+}
+
+.container.right-panel-active .sign-up-container {
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
+  animation: show 0.6s;
+}
+
+@keyframes show {
+  0%, 49.99% {
+    opacity: 0;
+    z-index: 1;
+  }
+  
+  50%, 100% {
+    opacity: 1;
+    z-index: 5;
+  }
+}
+
+.overlay-container {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  transition: transform 0.6s ease-in-out;
+  z-index: 100;
+}
+
+.container.right-panel-active .overlay-container{
+  transform: translateX(-100%);
+}
+
+.overlay {
+  /* background: #FF416C;
+  background: -webkit-linear-gradient(to right, #FF4B2B, #FF416C);
+  background: linear-gradient(to right, #FF4B2B, #FF416C); */
+  background: #333;
+  background: -webkit-linear-gradient(to right, #333, #767676);
+  background: linear-gradient(to right, #131313, #666666);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 0 0;
+  color: #FFFFFF;
+  position: relative;
+  left: -100%;
+  height: 100%;
+  width: 200%;
+  transform: translateX(0);
+  transition: transform 0.6s ease-in-out;
+}
+
+.container.right-panel-active .overlay {
+  transform: translateX(50%);
+}
+
+.overlay-panel {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 40px;
+  text-align: center;
+  top: 0;
+  height: 100%;
+  width: 50%;
+  transform: translateX(0);
+  transition: transform 0.6s ease-in-out;
+}
+
+.overlay-left {
+  transform: translateX(-20%);
+}
+
+.container.right-panel-active .overlay-left {
+  transform: translateX(0);
+}
+
+.overlay-right {
+  right: 0;
+  transform: translateX(0);
+}
+
+.container.right-panel-active .overlay-right {
+  transform: translateX(20%);
+}
+
+.social-container {
+  margin: 20px 0;
+}
+
+.social-container a {
+  border: 1px solid #DDDDDD;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  height: 40px;
+  width: 40px;
+}
+
+.customNextStepButton {
+  color: #FFFFFF;
+  margin-top: 10px;
+  background-color: #333;
+  /* border: 1px solid #FF4B2B; */
+  border: 1px solid #333;
+  padding: 8px;
+  border-radius: 20px;
+  margin-left: 206px;
+}
+
+.customNextStepButton:hover {
+  /* color: #FF4B2B; */
+  color: #333;
+  background-color: #FFFFFF;
+  /* border: 1px solid #FF4B2B; */
+  border: 1px solid #333;
+  transform: scale(1.07);
+}
+
+.customPreviousStepButton {
+  color: #FFFFFF;
+  margin-top: 10px;
+  /* background-color: #FF4B2B; */
+  background-color: #333;
+  /* border: 1px solid #FF4B2B; */
+  border: 1px solid #333;
+  padding: 8px;
+  border-radius: 20px;
+  margin-right: 0;
+}
+
+.customPreviousStepButton:hover {
+  /* color: #FF4B2B; */
+  color: #333;
+  background-color: #FFFFFF;
+  /* border: 1px solid #FF4B2B; */
+  border: 1px solid #333;
+  transform: scale(1.07);
+}
+
 
 </style>
