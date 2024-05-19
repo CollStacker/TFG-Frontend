c2f570e1f1cc6775ce848e89df68b7032a030088
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-05-19 20:20:59 +0100

    added functionality to find last 20 products posetd

--- src/components/OverviewComponent.vue
+++ src/components/OverviewComponent.vue
@@ -1,10 +1,50 @@
 <template>
   <div class="overviewComponentContainer">
-    <h1>COMENZAMOS LA VAINA LOCA</h1>
+    <!-- {{ last20Products?.length }} -->
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref, onMounted} from 'vue'
+import Toast from 'primevue/toast';
+import { userAuthentication } from '@/store/userAuth.store';
+import { useToast } from "primevue/usetoast";
+import { useRouter } from 'vue-router';
+import { type HomeViewProductDataInterface } from '@/types/product';
+import { type UserInterface } from '@/types/user';
+import { API_URI } from '@/types/env';
+
+const toast = useToast();
+const authStore = userAuthentication();
+const router = useRouter();
+
+onMounted(async () => {
+  await findProducts();
+});
+
+const last20Products = ref<HomeViewProductDataInterface[]>();
+const last20ProductsUser = ref<UserInterface[]>([]);
+
+const findProducts = async () => {
+  if(!await authStore.checkToken()) {
+    router.push('/');
+  } else {
+    const response = await fetch(API_URI + '/products', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${authStore.getToken()}`,
+      },
+    }) 
+    if (!response.ok) {
+      toast.add({ severity: 'error', summary: 'Error Message', detail: 'Failed finding products.', life: 3000 });
+    } else {
+      last20Products.value = await response.json();
+    }
+  }
+}
+
+
 </script>
 
 <style scoped>
