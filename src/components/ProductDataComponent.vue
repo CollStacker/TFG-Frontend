8e9991356df601f9b031bc26b1740a9f371b5228
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-05-19 11:02:23 +0100

    painting product image

--- src/components/ProductDataComponent.vue
+++ src/components/ProductDataComponent.vue
@@ -1,6 +1,18 @@
 <template>
   <Button class="editCollectionButton pi pi-arrow-left" @click="emitCloseProductComponent()"/>
-  {{ props.selectedProduct }}
+  <div class="productContainer" v-if="props.selectedProduct" style="margin-top: 20px; margin-bottom: 20px;">
+    <div class="productDataContainer">
+      <lightgallery :settings="{ speed: 500, plugins: plugins }" @init="onInit" @beforeSlide="onBeforeSlide" class="imageContainer">
+        <a v-if="props.selectedProduct.image !== ''" :href="props.selectedProduct.image">
+          <img alt="Collection image" :src="props.selectedProduct.image" class="lightGalleryImg"/>
+        </a>
+        <a v-else href="../assets/imgs/logo.png">
+          <img alt="No image" src="../assets/imgs/logo_without_background.png" class="lightGalleryImg"/>
+        </a>
+      </lightgallery>
+    </div>
+  </div>
+  <!-- {{ props.selectedProduct }} -->
 </template>
 
 <script setup lang="ts">
@@ -25,7 +37,44 @@ const emitCloseProductComponent = () => {
   emits("emitCloseProductComponent");
 }
 
+
+// GALERY
+import Lightgallery from 'lightgallery/vue';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-zoom.css';
+
+const plugins = [lgThumbnail, lgZoom];
+let lightGallery: any = null;
+
+const gallerySettings = {
+  speed: 500,
+  plugins: [lgZoom, lgThumbnail],
+};
+
+const onInit = (detail:any) => {
+  lightGallery = detail.instance;
+};
+
+const onBeforeSlide = () => {
+
+};
+
 </script>
 
 <style scoped>
+.productContainer {
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+}
+
+.productDataContainer {
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+}
 </style>
